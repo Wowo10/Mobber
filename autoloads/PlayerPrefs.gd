cd07317d5799286098bf28017ab9012823f96985
Wowo529 <wojtekwojtek529@gmail.com>
2026-06-22 22:07:50 +0200

    logo and basics.

--- autoloads/PlayerPrefs.gd
+++ autoloads/PlayerPrefs.gd
@@ -6,7 +6,7 @@ const SCHEME_PAD      := 2
 
 var archetype: int = 0  # 0 = Knight, 1 = Pirate
 var control_scheme: int = 0
-var mob_win_count: int = 120
+var mob_win_count: int = 50
 var peer_teams: Dictionary = {}  # peer_id -> 0 (Arena1/Team1) or 1 (Arena2/Team2)
 var room_code: String = ""
 var player_name: String = ""
